a11a9c997d364fb5d515c1f805b68e796f80d65d
anithasunil2008 <anithasunil2008@gmail.com>
2018-01-08 11:31:12 -0500

    finished program

---
+++ assets/javascript/game.js
@@ -0,0 +1,101 @@
+$(document).ready(function(){
+
+var guesses = $('#guesses');
+var finalTotal = $('#userTotal');
+var winScore = $('#wins');
+var lossesScore = $('#losses');
+
+var wins = 0;
+var losses = 0;
+
+var resetRandom = function() {
+
+    resetUserTotal();
+    random = Math.floor((Math.random() * 100) + 19);
+    randomHexa = Math.floor((Math.random() * 12) + 1);
+    randomDiamond = Math.floor((Math.random() * 12) + 1);
+    randomOcta = Math.floor((Math.random() * 12) + 1);
+    randomSquare = Math.floor((Math.random() * 12) + 1);
+
+    guesses.text(random);
+    console.log(random);
+
+}
+
+var resetUserTotal = function() {
+    finalTotal.text(0);
+    userTotal = 0;
+
+}
+
+//winner function
+var win = function() {
+    wins++;
+    winScore.text(wins);
+    resetRandom();
+}
+
+//loser function
+var loser = function() {
+    losses++;
+    lossesScore.text(losses);
+    resetRandom();
+}
+
+//hexa button onclick function
+$("#hexa").on("click", function() {
+
+    userTotal = userTotal + randomHexa;
+    finalTotal.text(userTotal);
+    console.log(randomHexa);
+    if (random == userTotal) {
+        win();
+    } else if (random < userTotal) {
+        loser();
+    }
+});
+
+//diamond button onclick function
+$("#diamond").on("click", function() {
+
+    userTotal = userTotal + randomDiamond;
+    finalTotal.text(userTotal);
+    console.log(randomDiamond);
+    if (random == userTotal) {
+        win();
+    } else
+    if (random < userTotal) {
+        loser();
+    }
+});
+
+//octa button onclick function
+$("#octa").on("click", function() {
+
+    userTotal = userTotal + randomOcta;
+    finalTotal.text(userTotal);
+    console.log(randomOcta);
+    if (random == userTotal) {
+        win();
+    } else
+    if (random < userTotal) {
+        loser();
+    }
+});
+
+//square button onclick function
+$("#square").on("click", function() {
+
+    userTotal = userTotal + randomSquare;
+    finalTotal.text(userTotal);
+    console.log(randomSquare);
+    if (random == userTotal) {
+        win();
+    } else
+    if (random < userTotal) {
+        loser();
+    }
+});
+
+resetRandom();
+});
